5605b018e3463352b182aa8d4d52d512957c83d8
alexyd88 <alexyd888@gmail.com>
2024-11-25 15:08:16 -0500

    can get advice from a person's conversations

--- analyze.py
+++ analyze.py
@@ -26,6 +26,15 @@ def send_prompt(prompt):
     )
     return chat_completion.choices[0].message.parsed
 
+def get_overall_conversation_score(score):
+    return (score.affection + score.vulnerability + score.kindness + score.other - score.negative) / 4
+
 def ask_match(conversation):
-    prompt = open("prompt.txt", "r").read()
+    prompt = open("conversation_prompt.txt", "r").read()
     return send_prompt(prompt + conversation)
+
+def ask_advice(conversations):
+    prompt = open("person_prompt.txt", "r").read()
+    for i in conversations:
+        prompt += "\n\nConversation:\n" + i
+    return send_prompt(prompt)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from analyze import ask_match
+from analyze import ask_match, ask_advice
 
 if __name__ == "__main__":
     convo = open("example_convo.txt", "r").read()
     result = ask_match(convo)
     print(result.explanation)
     print("\n\n\n\n")
+    print(result)
+
+    print("\n\n\nTips\n")
+    result = ask_advice([convo])
     print(result)
